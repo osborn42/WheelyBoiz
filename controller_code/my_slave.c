dd5acac918de1cbc1873c36c471f967ddbc64689
muelle61 <46976874+muelle61@users.noreply.github.com>
2020-11-12 16:29:27 -0500

    Update my_slave.c
    Updated the pins for GPIO and ADC

--- controller_code/my_slave.c
+++ controller_code/my_slave.c
@@ -5,8 +5,8 @@
 #include <driver/adc.h>
 #include "espnow_basic_config.h"
 
-#define GPIO_INPUT_0	2
-#define GPIO_INPUT_1	4
+#define GPIO_INPUT_0	14   //horn
+#define GPIO_INPUT_1	12   //lights
 
 static const char *TAG = "My_Slave";
 int switchFlickedState = 0;
@@ -15,14 +15,22 @@ int history = 0;
 // Your function to populate a my_data_t to send
 void my_data_populate(my_data_t *data)
 {
+
+/*  If need to configure the adc channels here is some code
+    adc1_config_width(ADC_WIDTH_12Bit);
+	adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_11db);  //xCar
+	adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_11db);  //yCar
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_11db);  //xCam
+    
+*/
     ESP_LOGI(TAG, "Populating my_data_t");
 
     data->button_pushed = gpio_get_level(GPIO_INPUT_0);
     printf("test: %d \n", gpio_get_level(GPIO_INPUT_0));
 
-    int yvalue = adc1_get_raw(ADC1_CHANNEL_7);
-    int xvalue = adc1_get_raw(ADC1_CHANNEL_6);
-    int xvalue2 = adc1_get_raw(ADC1_CHANNEL_5);
+    int yvalue = adc1_get_raw(ADC1_CHANNEL_7);  //xCar
+    int xvalue = adc1_get_raw(ADC1_CHANNEL_6);  //yCar
+    int xvalue2 = adc1_get_raw(ADC1_CHANNEL_4); //xCam
 
     printf("xvalue: %d\n", xvalue);
     printf("yvalue: %d\n", yvalue);
